bb404e5e21e3dd2441ad7708a52da084c98949f5
Ben Johnson <monk.sound.design@gmail.com>
2024-03-23 19:38:58 -0400

    tw theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,20 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        text: "#0c1817",
+        bg: "#f1f8f6",
+        primary: "#59aba3",
+        secondary: "#aaa7d3",
+        accent: "#9882c0",
+        text_dark: "#e7f3f2",
+        bg_dark: "#070e0c",
+        primary_dark: "#54a69e",
+        secondary_dark: "#2f2c58",
+        accent_dark: "#553f7d",
       },
     },
   },
